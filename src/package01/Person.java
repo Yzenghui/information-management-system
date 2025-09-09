8bab2eaed4b9cfea8b48f9e14cebb6cfd9c60f9b
YuanZengHui <2814579785@qq.com>
2025-09-09 12:46:41 +0800

    feat: 集成数据库连接与DAO层实现
    - 添加Druid连接池配置
    - 实现AccountDAOJdbc用户数据访问
    - 实现PersonDAOJdbc人员信息CRUD操作
    - 完善学生教师信息管理系统功能

--- src/package01/Person.java
+++ src/package01/Person.java
@@ -3,16 +3,18 @@ package package01;
 public class Person {
     private String name;   //姓名
     private String gender; //性别
+    private String profession;//职业
     private String address;//地址
     
     public Person() {
     	
     }
     
-	public Person(String name, String gender, String address) {
+	public Person(String name, String gender,String profession,String address) {
 		super();//默认先访问父类的无参构造
 		this.name = name;
 		this.gender = gender;
+		this.profession=profession;
 		this.address = address;
 	}
 
@@ -40,10 +42,16 @@ public class Person {
 		this.address = address;
 	}
 
+	public String getProfession() {
+		return profession;
+	}
+
+	public void setProfession(String profession) {
+		this.profession = profession;
+	}
+
 	@Override
 	public String toString() {
-		return "姓名" + getName() + "  性别" + getGender() + "  籍贯" + getAddress();
+		return "姓名：" + getName() + "  性别：" + getGender() + "  职业：" + getProfession() + "  籍贯：" + getAddress();
 	}
-    
-    
 }
--- src/package01/Teacher.java
+++ src/package01/Teacher.java
@@ -2,14 +2,16 @@ package package01;
 
 public class Teacher extends Person{
     private String id;//工号
+    private String subject;//所授学科
     
     public Teacher() {
     	
     }
 
-	public Teacher(String name,String gender,String address,String id) {
-		super(name,gender,address);
+	public Teacher(String name,String gender,String profession,String address,String id,String subject) {
+		super(name,gender,profession,address);
 		this.id = id;
+		this.subject=subject;
 	}
     
 	public String getId() {
@@ -20,8 +22,16 @@ public class Teacher extends Person{
 		this.id = id;
 	}
 
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
 	@Override
 	public String toString() {
-		return "教师姓名" + getName() + "  性别" + getGender() + "  籍贯" + getAddress()+"  工号："+getId();
+		return "工号：" + getId() + "  教师姓名：" + getName() + "  性别：" + getGender() + "  所授学科：" + getSubject()+ "  职业：" + getProfession() + "  籍贯：" + getAddress();
 	}
 }
