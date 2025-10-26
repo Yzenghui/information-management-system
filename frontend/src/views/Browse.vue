87800cbd861c2f495f50163bca7acf1f25752583
YuanZengHui <2814579785@qq.com>
2025-10-26 20:17:40 +0800

    feat: 为信息浏览页面添加响应式支持
    - 添加屏幕宽度监听和自适应布局
    - 实现表格尺寸和列宽动态调整
    - 优化移动端表格显示效果
    - 保持所有数据列完整显示

--- frontend/src/views/Browse.vue
+++ frontend/src/views/Browse.vue
@@ -10,23 +10,38 @@
     当 loading = false 时，加载动画消失 -->
     <el-tabs v-model="activeTab" @tab-click="handleTabClick">
       <el-tab-pane label="学生信息" name="students">
-        <el-table :data="studentList" v-loading="loading">
-          <el-table-column prop="id" label="学号" width="120"></el-table-column>
-          <el-table-column prop="name" label="姓名" width="100"></el-table-column>
-          <el-table-column prop="gender" label="性别" width="80"></el-table-column>
-          <!-- 没指定宽度的列自动平分剩余空间 -->
-          <el-table-column prop="major" label="专业"></el-table-column>
-          <el-table-column prop="address" label="籍贯"></el-table-column>
+        <el-table 
+          :data="studentList" 
+          v-loading="loading"
+          :size="tableSize"
+        >
+          <el-table-column prop="id" label="学号" :width="columnWidths.id"></el-table-column>
+          <el-table-column prop="name" label="姓名" :width="columnWidths.name"></el-table-column>
+          <el-table-column prop="gender" label="性别" :width="columnWidths.gender"></el-table-column>
+          <el-table-column prop="major" label="专业" :width="columnWidths.major"></el-table-column>
+          <el-table-column 
+            prop="address" 
+            label="籍贯" 
+            :width="columnWidths.address"
+          ></el-table-column>
         </el-table>
       </el-tab-pane>
       
       <el-tab-pane label="教师信息" name="teachers">
-        <el-table :data="teacherList" v-loading="loading">
-          <el-table-column prop="id" label="工号" width="120"></el-table-column>
-          <el-table-column prop="name" label="姓名" width="100"></el-table-column>
-          <el-table-column prop="gender" label="性别" width="80"></el-table-column>
-          <el-table-column prop="subject" label="所授学科"></el-table-column>
-          <el-table-column prop="address" label="籍贯"></el-table-column>
+        <el-table 
+          :data="teacherList" 
+          v-loading="loading"
+          :size="tableSize"
+        >
+          <el-table-column prop="id" label="工号" :width="columnWidths.id"></el-table-column>
+          <el-table-column prop="name" label="姓名" :width="columnWidths.name"></el-table-column>
+          <el-table-column prop="gender" label="性别" :width="columnWidths.gender"></el-table-column>
+          <el-table-column prop="subject" label="所授学科" :width="columnWidths.major"></el-table-column>
+          <el-table-column 
+            prop="address" 
+            label="籍贯" 
+            :width="columnWidths.address"
+          ></el-table-column>
         </el-table>
       </el-tab-pane>
     </el-tabs>
@@ -40,6 +55,7 @@ export default {
     return {
       activeTab: "students",
       loading: false,
+      screenWidth: document.documentElement.clientWidth, // 获取屏幕宽度用于响应式
       studentList: [
         {
           id: "2021001",
@@ -67,6 +83,44 @@ export default {
       ]
     };
   },
+  computed: {
+    // 根据屏幕宽度动态计算表格尺寸
+    tableSize() {
+      return this.screenWidth < 768 ? 'small' : 'medium';
+    },
+    // 根据屏幕宽度动态计算列宽
+    columnWidths() {
+      // major、address两列自动平分剩余空间
+      if (this.screenWidth < 480) {
+        // 手机端：紧凑布局
+        return {
+          id: '100',
+          name: '80', 
+          gender: '60',
+          major: '',
+          address: ''
+        };
+      } else if (this.screenWidth < 768) {
+        // 平板端：适中布局
+        return {
+          id: '110',
+          name: '90',
+          gender: '70', 
+          major: '',
+          address: ''
+        };
+      } else {
+        // 电脑端：宽松布局
+        return {
+          id: '120',
+          name: '100',
+          gender: '80',
+          major: '',
+          address: ''
+        };
+      }
+    }
+  },
   methods: {
     handleTabClick() {
       // 切换选项卡时可以重新加载数据
@@ -78,16 +132,40 @@ export default {
       setTimeout(() => {
         this.loading = false;
       }, 500);
+    },
+    // 监听窗口大小变化
+    handleResize() {
+      this.screenWidth = document.documentElement.clientWidth;
     }
   },
   mounted() {
     this.loadData();
+    // 添加窗口大小变化监听
+    window.addEventListener('resize', this.handleResize);
+  },
+  beforeDestroy() {
+    // 移除监听器，防止内存泄漏
+    window.removeEventListener('resize', this.handleResize);
   }
 };
 </script>
 
 <style scoped>
 .browse-page {
+  /* 根据屏幕宽度动态调整内边距 */
   padding: 20px;
+}
+
+/* 响应式样式 */
+@media screen and (max-width: 768px) {
+  .browse-page {
+    padding: 15px;
+  }
+}
+
+@media screen and (max-width: 480px) {
+  .browse-page {
+    padding: 10px;
+  }
 }
 </style>
